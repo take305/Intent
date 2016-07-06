315419e7d096d05345be041fc5f9a4b7f324616a
unknown <>
2016-07-07 01:41:18 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/takefumi/onishi/intent/MainActivity.java
@@ -0,0 +1,31 @@
+package jp.techacademy.takefumi.onishi.intent;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText mEditText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mEditText = (EditText)findViewById(R.id.editText);
+        Button button=(Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_SEND);
+                intent.setType("text/plain");
+                intent.putExtra(Intent.EXTRA_TEXT,mEditText.getText().toString());
+                startActivity(intent);
+            }
+        });
+    }
+}
